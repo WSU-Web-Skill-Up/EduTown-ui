c59cea237dc5e801dcfd239359eae8c3eec4f549
82256977 <chanho.jeong@kt.com>
2022-11-28 10:57:30 +0900

    라우터 설정

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -52,47 +52,47 @@
                         <i class="ni ni-collection d-lg-none"></i>
                         <span class="nav-link-inner--text">주변공연정보</span>
                     </a>
-                    <router-link to="/landing" class="dropdown-item">지역별 공연</router-link>
-                    <router-link to="/profile" class="dropdown-item">뮤지컬</router-link>
-                    <router-link to="/login" class="dropdown-item">콘서트</router-link>
-                    <router-link to="/login" class="dropdown-item">연극</router-link>
-                    <router-link to="/register" class="dropdown-item">전시회</router-link>
-                    <router-link to="/register" class="dropdown-item">기타</router-link>
+                    <router-link to="/locshow" class="dropdown-item">지역별 공연</router-link>
+                    <router-link to="/musical" class="dropdown-item">뮤지컬</router-link>
+                    <router-link to="/concert" class="dropdown-item">콘서트</router-link>
+                    <router-link to="/theater" class="dropdown-item">연극</router-link>
+                    <router-link to="/exhibition" class="dropdown-item">전시회</router-link>
+                    <router-link to="/etcshow" class="dropdown-item">기타</router-link>
                 </base-dropdown>
                 <base-dropdown tag="li" class="nav-item">
                     <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
                         <i class="ni ni-collection d-lg-none"></i>
                         <span class="nav-link-inner--text">게시판</span>
                     </a>
-                    <router-link to="/landing" class="dropdown-item">자유게시판</router-link>
-                    <router-link to="/login" class="dropdown-item">공연모집게시판</router-link>
-                    <router-link to="/register" class="dropdown-item">거래게시판</router-link>
+                    <router-link to="/freeboard" class="dropdown-item">자유게시판</router-link>
+                    <router-link to="/recruitboard" class="dropdown-item">공연모집게시판</router-link>
+                    <router-link to="/dealboard" class="dropdown-item">거래게시판</router-link>
                 </base-dropdown>
             </ul>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank" rel="noopener"
+                    <a class="nav-link nav-link-icon" href="https://www.facebook.com" target="_blank" rel="noopener"
                        data-toggle="tooltip" title="Like us on Facebook">
                         <i class="fa fa-facebook-square"></i>
                         <span class="nav-link-inner--text d-lg-none">Facebook</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.instagram.com/creativetimofficial"
+                    <a class="nav-link nav-link-icon" href="https://www.instagram.com"
                        target="_blank" rel="noopener" data-toggle="tooltip" title="Follow us on Instagram">
                         <i class="fa fa-instagram"></i>
                         <span class="nav-link-inner--text d-lg-none">Instagram</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank" rel="noopener"
+                    <a class="nav-link nav-link-icon" href="https://twitter.com" target="_blank" rel="noopener"
                        data-toggle="tooltip" title="Follow us on Twitter">
                         <i class="fa fa-twitter-square"></i>
                         <span class="nav-link-inner--text d-lg-none">Twitter</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://github.com/creativetimofficial/vue-argon-design-system"
+                    <a class="nav-link nav-link-icon" href="https://https://github.com/WSU-Web-Skill-Up"
                        target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
                         <i class="fa fa-github"></i>
                         <span class="nav-link-inner--text d-lg-none">Github</span>
@@ -101,12 +101,14 @@
                 <li class="nav-item d-none d-lg-block ml-lg-4">
                     <a target="_blank" rel="noopener"
                        class="btn btn-neutral btn-icon">
-                        <span class="btn-inner--icon">
+                        <!-- <span class="btn-inner--icon"> -->
                         <!-- <i class="fa fa-cloud-download mr-2"></i> -->
-                        </span>
+                        <!-- </span> -->
                     <!-- <router-link to="/login" class="dropdown-item">공연모집게시판</router-link> -->
                         
-                        <span to="/login" class="dropdown-item">로그인</span>
+                        <!-- <span to="/login" class="dropdown-item">로그인</span> -->
+                        <span to="/login">로그인</span>
+
                     </a>
                 </li>
             </ul>
